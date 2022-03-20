3e7e54424739f3a2f7301f681eb584fd6d0530a9
Michael Keene <93222787+mikekeene@users.noreply.github.com>
2022-03-19 23:13:18 -0500

    Update tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,16 +11,12 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbTagData => res.json(dbTagData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  .then(tagData => res.json(tagData))
+  .catch((err) => res.status(500).json(err));
 });
 
 
 router.get('/:id', (req, res) => {
-
   Tag.findOne({
     where: {
       id: req.params.id
@@ -32,28 +28,16 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-    .then(dbTagData => {
-      if (!dbTagData) {
-        res.status(404).json({ message: 'No tag found with this id'});
-        return;
-      }
-      res.json(dbTagData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  .then(tagData => res.json(tagData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
   Tag.create({
     tag_name: req.body.tag_name
   })
-    .then(dbTagData => res.json(dbTagData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-  });
+  .then(tagData => res.json(tagData))
+  .catch((err) => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
@@ -62,18 +46,8 @@ router.put('/:id', (req, res) => {
         id: req.params.id
     }
   })
-    .then(dbTagData => {
-        if (!dbTagData[0]) {
-            res.status(404).json({ message: 'No tag found with this id'});
-            return;
-        }
-        res.json(dbTagData);
-  })
-    .catch(err => {
-        console.log(err); 
-        res.status(500).json(err);
-  });
-
+  .then(tagData => res.json(tagData))
+  .catch((err) => res.status(500).json(err));
 });
 
 
@@ -83,17 +57,8 @@ router.delete('/:id', (req, res) => {
         id: req.params.id
     }
   })
-    .then(dbTagData => {
-        if (!dbTagData) {
-            res.status(404).json({ message: 'No tag found with this id'});
-            return;
-        }
-        res.json(dbTagData);
-  })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-  });
+  .then(tagData => res.json(tagData))
+  .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
